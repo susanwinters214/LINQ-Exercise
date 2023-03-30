cd34f72ee421f8f154985658b00b9461d95389d5
Susan Winters <susanwinters214@gmail.com>
2023-03-29 20:29:21 -0500

    setting up game names

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a list of video game names...
+            var myVideoGames = new[] { "AnimalGame", "DrinkingGame", "SportsGame", "DancingGame", "PokerGame" };
+
+
+            //Order the list of games by length of the game name.
+           // .Orderby(myVideoGames.gamename)
+
+            //Use the lambda expression in this exercise as well.
+
+
+            //use Method Syntax for this exercise
+
+
+
+
+
+
         }
     }
 }
